253809b1f3bceb93ebc8fbafdab5bf02939cd384
zxowl <zxowl@qq.com>
2019-03-21 17:28:20 +0800

    2019-03-21 17:28:00--tzhu 创建一个类文件

---
+++ src/main/java/com/ssm/teamgys/svntest/TestStu010.java
@@ -0,0 +1,21 @@
+package com.ssm.teamgys.svntest;
+
+import java.sql.Time;
+
+/**
+ * 2019-03-21 17:27:24
+ * tzhu编写的代码类
+ */
+public class TestStu010 {
+
+    public static void main(String[] args) {
+
+        //  2019-03-21 17:27:09
+        System.out.println("   全球  智~~~ ");
+
+    }
+
+
+
+
+}

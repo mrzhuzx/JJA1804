9ab5ba0ec538120a21aa20897a0985d8cdd70c3a
wujiayi5354 <308043307@qq.com>
2019-03-21 16:42:01 +0800

    点一次，玩一年

---
+++ src/main/java/com/ssm/teamgys/svntest/wjyTest.java
@@ -0,0 +1,7 @@
+package com.ssm.teamgys.svntest;
+
+public class wjyTest {
+    public static void main(String[] args) {
+        System.out.println("###########################10###################");
+    }
+}

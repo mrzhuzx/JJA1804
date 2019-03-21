0ea48b2296efac6cb8b8b9080593ed9503127a4d
Administrator <2224145325@>
2019-03-21 19:26:52 +0800

    游的第er次提交

--- src/main/java/com/ssm/teamgys/svntest/TestYou.java
+++ src/main/java/com/ssm/teamgys/svntest/TestYou.java
@@ -4,6 +4,7 @@ public class TestYou {
     public static void main(String[] args) {
         System.out.println("游奋前");
         System.out.println("111");
-        System.out.println("1562551");
+        System.out.println("1562551 ");
+        System.out.println("测试");
     }
 }

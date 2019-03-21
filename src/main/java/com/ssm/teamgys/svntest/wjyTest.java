d5b67a589e1719246b33c08a90e6de7a19380fec
wujiayi5354 <308043307@qq.com>
2019-03-21 16:50:00 +0800

    点一次，玩一年

--- src/main/java/com/ssm/teamgys/svntest/wjyTest.java
+++ src/main/java/com/ssm/teamgys/svntest/wjyTest.java
@@ -3,5 +3,7 @@ package com.ssm.teamgys.svntest;
 public class wjyTest {
     public static void main(String[] args) {
         System.out.println("###########################10###################");
+        //  https://github.com/mrzhuzx/JJA1804.git
+        // https://github.com/mrzhuzx/JJA1804.git
     }
 }

4aa75099e3c1b3b24a4ba6633123446741426cca
Administrator <1720962711@qq.com>
2019-03-21 16:50:31 +0800

    0321_lxh_创建了一个test类

--- src/main/java/com/ssm/teamgys/svntest/TestLxh.java
+++ src/main/java/com/ssm/teamgys/svntest/TestLxh.java
@@ -6,5 +6,6 @@ package com.ssm.teamgys.svntest;
 public class TestLxh {
     public static void main(String[] args) {
         System.out.println("----____----____");
+        System.out.println("----____----____");
     }
 }

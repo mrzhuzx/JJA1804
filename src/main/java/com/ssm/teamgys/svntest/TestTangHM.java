38bd4eb729cd4c5be41f959c48af0f439619222a
Gitshirothm <2750193247@qq.com>
2019-03-21 16:31:19 +0800

    这是汤华铭Test2

--- src/main/java/com/ssm/teamgys/svntest/TestTangHM.java
+++ src/main/java/com/ssm/teamgys/svntest/TestTangHM.java
@@ -8,5 +8,6 @@ import javax.xml.bind.SchemaOutputResolver;
 public class TestTangHM {
     public static void main(String[] args) {
         System.out.println("第二次测试");
+        System.out.println("第二次测试");
     }
 }

0a5a5bd60b0242fa0fc59a393b12dd8233098a92
wujiayi5354 <308043307@qq.com>
2019-03-21 17:01:37 +0800

    点一次，玩一年

--- src/main/java/com/ssm/teamgys/svntest/TestLzp.java
+++ src/main/java/com/ssm/teamgys/svntest/TestLzp.java
@@ -3,7 +3,7 @@ package com.ssm.teamgys.svntest;
 public class TestLzp {
     public static void main(String[] args) {
         System.out.println("我愿称你为，最强！");
-        System.out.println("你见过我的全盛期吗？");
+        System.out.println("你见过我的全盛期吗？"+"没有");
         System.out.println("你要跟我起舞吗");
     }
 }

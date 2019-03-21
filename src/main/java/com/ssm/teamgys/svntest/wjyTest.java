da89df1adc7fe0d29c2f03c36655959bd8e6bbba
wujiayi5354 <308043307@qq.com>
2019-03-21 17:04:56 +0800

    点一次，玩一年

--- src/main/java/com/ssm/teamgys/svntest/wjyTest.java
+++ src/main/java/com/ssm/teamgys/svntest/wjyTest.java
@@ -6,5 +6,8 @@ public class wjyTest {
     public static void main(String[] args) {
         System.out.println("###########################10###################");
 
+        for (int i = 0; i <6; i++) {
+            System.out.println("这是第"+i+"次更新");
+        }
     }
 }

2619097c88d9b2afc88641dafa16c4b924b0dbe8
zxowl <zxowl@qq.com>
2019-03-22 17:34:08 +0800

    2019-03-22 17:33:42 JPA curd  Dog对象

--- src/main/java/com/ssm/teamgys/entity/Dog.java
+++ src/main/java/com/ssm/teamgys/entity/Dog.java
@@ -14,8 +14,11 @@ public class Dog {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)//自增主键
   private Integer dogId;
+  @Column
   private String dogName;
+  @Column
   private String  dogHobby;
+  @Column
   private Integer  dogAge;
 
 

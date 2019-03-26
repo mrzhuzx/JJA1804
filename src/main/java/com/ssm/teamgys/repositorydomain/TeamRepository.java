283e6f3412e4b9c15186a57a7202f09d98db6a4d
cspiderman <chenzefengboy@Sina.com>
2019-03-26 09:41:52 +0800

    陈泽锋tiger提交

---
+++ src/main/java/com/ssm/teamgys/repositorydomain/TeamRepository.java
@@ -0,0 +1,13 @@
+package com.ssm.teamgys.repositorydomain;
+
+import com.ssm.teamgys.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * desc:
+ * author:czf
+ * time:2019\3\25 0025
+ * version:
+ */
+public interface TeamRepository extends JpaRepository<Team,String> {
+}

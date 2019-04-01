f77853225f2829b437f16c1c41649548bdf1fcfe
Gitshirothm <2750193247@qq.com>
2019-04-01 17:58:30 +0800

    查询和添加、删除 2019-04-01 17:58:20

--- src/main/java/com/ssm/teamgys/repositorydomain/OrderDetailRepository.java
+++ src/main/java/com/ssm/teamgys/repositorydomain/OrderDetailRepository.java
@@ -12,8 +12,6 @@ import org.springframework.data.jpa.repository.Query;
  * 2019/3/23 0023
  */
 public interface OrderDetailRepository extends JpaRepository<OrderDetail,String> {
-    @Query("select OrderDetail from where detailId=?1")
-     void findOne(String detailId);
-
+  
 
 }

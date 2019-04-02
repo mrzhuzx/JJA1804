e0165458641ff51e31de042f4067175b05623054
Administrator <1720962711@qq.com>
2019-04-02 09:22:17 +0800

    Merge branch 'master' of D:\ideaworkpass\JJA1804 with conflicts.

--- src/main/java/com/ssm/teamgys/service/ProductCategoryService.java
+++ src/main/java/com/ssm/teamgys/service/ProductCategoryService.java
@@ -4,13 +4,14 @@ import com.ssm.teamgys.appcomm.BaseService;
 import com.ssm.teamgys.domain.ProductCategory;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public interface ProductCategoryService extends BaseService<ProductCategory> {
 
-void update(String categoryId, String categoryName );
+int update(String categoryId, String categoryName, Integer categoryType  );
     ProductCategory findbyid(String categoryId);
 
 
